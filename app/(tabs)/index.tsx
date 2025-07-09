1048d75d73817e3af64ae54bece78900377466f7
HieuNQ2003 <hieunqhe170048@fpt.edu.vn>
2025-07-09 21:37:11 +0700

    Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,129 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  FlatList,
+  Keyboard,
+  Pressable,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { Menu } from "react-native-paper";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+interface ITodo {
+  id: number;
+  text: string;
+  status: String;
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default function HomeScreen() {
+  const [todo, setTodo] = useState<string>("");
+  const [todoList, setTodoList] = useState<ITodo[]>([]);
+  const [show, setShow] = useState<boolean>(false);
+  const [status, setStatus] = useState<string>("Chưa làm");
+  const [menuVisible, setMenuVisible] = useState(false);
+  const statuses = ["Chưa làm", "Đang làm", "Đã hoàn thành"];
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      {/* Header */}
+      <View>
+        <View className="bg-orange-600 mt-20">
+          <Text className="text-4xl text-center font-bold">ToDo</Text>
+        </View>
+
+        {/* Input */}
+        <View className="p-3 flex flex-row">
+          {/* Nhập Nội Dung Công Việc */}
+          <TextInput
+            className="border-b-2 border-green-500 w-3/4 p-2"
+            onChangeText={(text) => setTodo(text)}
+            value={todo}
+            placeholder="Nhập nội dung công việc"
+          />
+          {/* Chọn Trạng Thái */}
+          <Pressable className="w-1/4">
+            <Menu
+              visible={menuVisible}
+              onDismiss={() => setMenuVisible(false)}
+              anchor={
+                <Pressable onPress={() => setMenuVisible(true)}>
+                  <Text className="border-1 ml-5 p-2">{status}</Text>
+                </Pressable>
+              }
+            >
+              {statuses.map((e, index) => (
+                <Menu.Item
+                  key={index}
+                  onPress={() => {
+                    setStatus(e);
+                    setMenuVisible(false);
+                  }}
+                  title={e}
+                />
+              ))}
+            </Menu>
+          </Pressable>
+        </View>
+        <Pressable className={show ? "" : "hidden"}>
+          <Text className="text-red-700"> Vui lòng nhập công việc cần làm</Text>
+        </Pressable>
+
+        <Pressable
+          className="bg-green-600 rounded-md p-2 m-3 items-center"
+          onPress={() => {
+            setShow(false);
+            if (!todo.trim()) {
+              setShow(true);
+              return;
+            }
+            setTodoList([
+              ...todoList,
+              { id: todoList.length + 1, text: todo, status },
+            ]);
+            setTodo("");
+            setStatus("Chưa làm");
+          }}
+        >
+          <Text className="font-bold">+Add</Text>
+        </Pressable>
+
+        {/* List */}
+        <View>
+          <FlatList
+            data={todoList}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <View className="flex flex-row items-center p-3 border-b border-gray-300">
+                <Text className="text-lg basis-4/12">{item.text}</Text>
+                <Text className="text-lg basis-4/12">{item.status}</Text>
+                <View className="basis-4/12 flex flex-row">
+                  <Pressable 
+                    className="bg-blue-600 rounded-md p-2 px-4"
+                    onPress={() => {
+                      
+                      setTodoList(updatedTodoList);
+                    }}
+                  >
+                    <Text className="text-white">Edit</Text>
+                  </Pressable>
+
+                  <Pressable
+                    className="bg-red-600 rounded-md p-2"
+                    onPress={() => {
+                      setTodoList(
+                        todoList.filter((todo) => todo.id !== item.id),
+                      );
+                    }}
+                  >
+                    <Text className="text-white">Delete</Text>
+                  </Pressable>
+                </View>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
